e189cc7d34ffdd1af944844c7368d656d098d09b
Leo Teng Liu <liutenghou@gmail.com>
2014-01-28 14:17:32 -0800

    version 1 with maps api working on client

--- ClinicsVancouver/src/com/google/gwt/sample/clinicsvancouver/client/ClinicsVancouver.java
+++ ClinicsVancouver/src/com/google/gwt/sample/clinicsvancouver/client/ClinicsVancouver.java
@@ -1,8 +1,13 @@
 package com.google.gwt.sample.clinicsvancouver.client;
 
+import com.google.gwt.maps.client.*;
+import com.google.gwt.maps.client.control.LargeMapControl;
+import com.google.gwt.maps.client.geom.LatLng;
+import com.google.gwt.maps.client.overlay.Marker;
 import com.google.gwt.sample.clinicsvancouver.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
@@ -11,8 +16,10 @@ import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -76,6 +83,14 @@ public class ClinicsVancouver implements EntryPoint {
 		dialogVPanel.add(closeButton);
 		dialogBox.setWidget(dialogVPanel);
 
+		//maps
+		Maps.loadMapsApi("", "2", false, new Runnable(){
+			public void run(){
+				buildUi();
+			}
+		});
+		
+		
 		// Add a handler to close the DialogBox
 		closeButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
@@ -148,5 +163,29 @@ public class ClinicsVancouver implements EntryPoint {
 		MyHandler handler = new MyHandler();
 		sendButton.addClickHandler(handler);
 		nameField.addKeyUpHandler(handler);
+	}
+	
+	private void buildUi(){
+		//Open a map centered on Cawker City, KS USA
+		LatLng cawkerCity = LatLng.newInstance(39.509,  -98.434);
+		
+		final MapWidget map = new MapWidget(cawkerCity, 2);
+		map.setSize("100%", "100%");
+		//Add some controls for the zoom level
+		map.addControl(new LargeMapControl());
+		
+		//Add a marker
+		map.addOverlay(new Marker(cawkerCity));
+		
+		//Add an info window to highlight a point of interest
+		map.getInfoWindow().open(map.getCenter(),
+				new InfoWindowContent("World's Largest Ball of Sisal Twine"));
+		
+		final DockLayoutPanel dock = new DockLayoutPanel(Unit.PX);
+		dock.addNorth(map, 500);
+		
+		//Add the map to the HTML host page
+		RootLayoutPanel.get().add(dock);
+		
 	}
 }
